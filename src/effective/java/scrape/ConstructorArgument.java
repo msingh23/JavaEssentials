2f0f6ba8e13ba768f6f9c8cfe386bed0780ce491
manu singh <manu.singh@ibm.com>
2017-03-26 13:33:55 -0400

    Basic Project Structure

---
+++ src/effective/java/scrape/ConstructorArgument.java
@@ -0,0 +1,27 @@
+/**
+ * 
+ */
+package effective.java.scrape;
+
+/**
+ * @author manu
+ *
+ */
+public class ConstructorArgument {
+
+	
+	//Overloaded Constructors can have same signature with jumbled order
+	// of the parameter in the argument list.
+	
+	//This creates confusion to the client using such API interface that which constructor does what.
+	public ConstructorArgument(String name, Integer number) {
+		// TODO Auto-generated constructor stub
+	}
+
+	public ConstructorArgument(Integer number, String name) {
+		// TODO Auto-generated constructor stub
+	}
+
+
+
+}
