833fd5fea7544b24fa0c88ff945dbd110891d378
manu singh <manu.singh@ibm.com>
2017-05-04 21:57:41 -0400

    refactor package

--- src/test/java/ctci/starter/AllTests.java
+++ src/test/java/ctci/starter/AllTests.java
@@ -4,9 +4,9 @@ import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 import org.junit.runners.Suite.SuiteClasses;
 
-import ctci.chap1.IsUnique2Test;
-import ctci.chap1.IsUnique3Test;
-import ctci.chap1.IsUniqueTest;
+import ctci.chap1.q1.IsUnique2Test;
+import ctci.chap1.q1.IsUnique3Test;
+import ctci.chap1.q1.IsUniqueTest;
 
 @RunWith(Suite.class)
 @SuiteClasses({ IsUnique2Test.class, IsUniqueTest.class, IsUnique3Test.class})
