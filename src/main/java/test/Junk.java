5f651f691dc0b37253773a5efc6a25591568f1cb
manu singh <manusingh.infinity@gmail.com>
2017-05-04 20:22:18 -0400

    adding the scrapejunk for test

---
+++ src/main/java/test/Junk.java
@@ -0,0 +1,23 @@
+/**
+ * 
+ */
+package test;
+
+/**
+ * @author manu
+ *
+ */
+public class Junk {
+	
+	public static void main(String[] args) {
+		
+		int vec = 0;
+		
+		vec = 1<<-1;
+		
+		System.out.println(vec);
+		System.out.println((2147483647+1));
+		
+	}
+	
+}
