0ce68ff1fae6528aa2423d574404e3a1341d58d8
manu singh <manu.singh@ibm.com>
2017-05-08 02:01:36 -0400

    PalindromePerm

--- src/test/java/ctci/starter/AllTests.java
+++ src/test/java/ctci/starter/AllTests.java
@@ -7,12 +7,13 @@ import org.junit.runners.Suite.SuiteClasses;
 import ctci.chap1.IsUnique2Test;
 import ctci.chap1.IsUnique3Test;
 import ctci.chap1.IsUniqueTest;
+import ctci.chap1.PalindromePermTest;
 import ctci.chap1.URLifyStringTest;
 import ctci.chap1.StringPermutationOfOtherTest;
 
 @RunWith(Suite.class)
 @SuiteClasses({ IsUnique2Test.class, IsUniqueTest.class, IsUnique3Test.class, 
-	StringPermutationOfOtherTest.class,URLifyStringTest.class})
+	StringPermutationOfOtherTest.class,URLifyStringTest.class, PalindromePermTest.class})
 public class AllTests {
 
 }
