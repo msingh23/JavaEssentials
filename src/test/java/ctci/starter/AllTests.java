c30a45d4a280a338e4f0b0b3380bb3d895d07248
manu singh <manu.singh@ibm.com>
2017-05-11 14:22:26 -0400

    simple short method

--- src/test/java/ctci/starter/AllTests.java
+++ src/test/java/ctci/starter/AllTests.java
@@ -7,13 +7,14 @@ import org.junit.runners.Suite.SuiteClasses;
 import ctci.chap1.IsUnique2Test;
 import ctci.chap1.IsUnique3Test;
 import ctci.chap1.IsUniqueTest;
+import ctci.chap1.OneEditDistanceStringTest;
 import ctci.chap1.PalindromePermTest;
 import ctci.chap1.URLifyStringTest;
 import ctci.chap1.StringPermutationOfOtherTest;
 
 @RunWith(Suite.class)
-@SuiteClasses({ IsUnique2Test.class, IsUniqueTest.class, IsUnique3Test.class, 
-	StringPermutationOfOtherTest.class,URLifyStringTest.class, PalindromePermTest.class})
+@SuiteClasses({ IsUnique2Test.class, IsUniqueTest.class, IsUnique3Test.class, StringPermutationOfOtherTest.class,
+		URLifyStringTest.class, PalindromePermTest.class, OneEditDistanceStringTest.class })
 public class AllTests {
 
 }
