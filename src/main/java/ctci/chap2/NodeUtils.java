246e7f77a37d4ac1591c955ffa0d9948bc3d9771
manu singh <manusingh.infinity@gmail.com>
2017-05-22 22:04:24 -0400

    Remove Duplicates

---
+++ src/main/java/ctci/chap2/NodeUtils.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ */
+package ctci.chap2;
+
+/**
+ * @author manu
+ *
+ */
+public class NodeUtils {
+
+	public static void printFromNode(Node data) {
+		Node temp = data;
+		while (temp.next != null) {
+			System.out.print(temp.data + "->");
+		}
+		System.out.print(temp.data);
+		System.out.println();
+	}
+
+}
