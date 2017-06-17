74b9cff4a5ed7213ec9fc95223f2700a5695862d
manu singh <manusingh.infinity@gmail.com>
2017-06-17 18:25:30 -0400

    MultiStack final

--- src/main/java/ctci/chap3/MultiStack.java
+++ src/main/java/ctci/chap3/MultiStack.java
@@ -7,68 +7,71 @@ package ctci.chap3;
  * @author manu
  *
  */
-public class MultiStack<T> {
+public class MultiStack {
 
 	public class StackInfo {
 		int start;
 		int end;
 		int capacity;
+		int size;
 
 		public StackInfo(int start, int capacity) {
 			this.start = start;
 			this.capacity = capacity;
 			end = start - 1;
+			size = 0;
 		}
 
 	}
 
-	T value[];
+	int value[];
 	StackInfo[] stack;
 
 	public MultiStack(int stackNo, int stackSize) {
 		// TODO Auto-generated constructor stub
-		stack = (MultiStack<T>.StackInfo[]) new Object[stackNo];
+		stack = new StackInfo[stackNo];
 
 		for (int i = 0; i < stackNo; i++) {
 			stack[i] = new StackInfo(i * stackNo, stackSize);
 		}
-		value = (T[]) new Object[stackNo * stackSize];
+		value = new int[stackNo * stackSize];
 	}
 
-	public void push(int stackNo, T data) throws Exception {
+	public void push(int stackNo, int data) throws Exception {
 
 		if (isMultiTaskFull()) {
 			throw new Exception("Stack is full");
 		}
 
-		if (stack[stackNo].end == stack[stackNo].capacity - 1) {
+		if (stack[stackNo].start + stack[stackNo].size == stack[stackNo].capacity) {
 			expand(stackNo);
 		}
-		value[stack[stackNo].end + 1] = data;
-		stack[stackNo].end = (stack[stackNo].end + 1) % value.length;
+		value[(stack[stackNo].size) % value.length] = data;
+		// stack[stackNo].end = (stack[stackNo].end + 1) % value.length;
+		stack[stackNo].size++;
 
 	}
 
-	public T pop(int stackNo) throws Exception {
-		T out;
-		if ((stack[stackNo].end - 1 + value.length) % value.length < stack[stackNo].start) {
+	public int pop(int stackNo) throws Exception {
+		int out;
+		if (stack[stackNo].size == 0) {
 			throw new Exception("Empty Stack");
 		}
-		out = value[stack[stackNo].end];
-		stack[stackNo].end = (stack[stackNo].end - 1 + value.length) % value.length;
+		out = value[(stack[stackNo].start + stack[stackNo].size - 1) % value.length];
+		stack[stackNo].size--;
 		return out;
 	}
 
-	public T peek(int stackNo) throws Exception {
-		T out;
-		out = value[stack[stackNo].end];
+	public int peek(int stackNo) throws Exception {
+		int out;
+		out = value[stack[stackNo].size - 1];
 		return out;
 	}
 
 	private void expand(int stackNo) {
 
 		int next = (stackNo + 1) % stack.length;
-		if (stack[next].end == stack[next].start + stack[next].capacity - 1) {
+		if (stack[next].size == stack[next].capacity) {
 			expand(next);
 			stack[next].capacity++;
 		}
@@ -86,7 +89,8 @@ public class MultiStack<T> {
 	private boolean isMultiTaskFull() {
 		int count = 0;
 		for (StackInfo st : stack) {
-			count += st.end;
+
+			count += st.size;
 		}
 		return count == value.length;
 	}
