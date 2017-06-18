416bb78a11e5af7abcd1d0934cdeeb734c50426f
manu singh <manusingh.infinity@gmail.com>
2017-06-17 23:50:25 -0400

    Stack min

--- src/main/java/ctci/chap3/StackMin.java
+++ src/main/java/ctci/chap3/StackMin.java
@@ -1,5 +1,47 @@
 package ctci.chap3;
 
 public class StackMin {
+	private StackNode head;
+
+	public class StackNode {
+		int data;
+		int min;
+		StackNode next;
+
+		public StackNode(int data) {
+			this.data = data;
+			this.min = data;
+			next = null;
+		}
+	}
+
+	public StackMin(int data) {
+		head = new StackNode(data);
+	}
+
+	public void push(int data) {
+		StackNode temp = new StackNode(data);
+		if (temp.min > head.min) {
+			temp.min = head.min;
+		}
+		head = temp;
+	}
+
+	public int pop() throws Exception {
+		if (head == null) {
+			throw new Exception("Stack is empty");
+		}
+		int out = head.data;
+		head = head.next;
+		return out;
+	}
+
+	public int peek() {
+		return head.data;
+	}
+
+	public int min() {
+		return head.min;
+	}
 
 }
