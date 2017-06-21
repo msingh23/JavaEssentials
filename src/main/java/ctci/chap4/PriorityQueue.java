d58a4ff00c40c7f1ce684554a716a2d4339c4db3
manu singh <manusingh.infinity@gmail.com>
2017-06-20 22:04:15 -0400

    priority queue implementation

--- src/main/java/ctci/chap4/PriorityQueue.java
+++ src/main/java/ctci/chap4/PriorityQueue.java
@@ -11,6 +11,8 @@ public class PriorityQueue<T> {
 
 	int capacity;
 	int size;
+	boolean min;
+	boolean max;
 	HeapNode[] heap;
 
 	public class HeapNode<T> {
@@ -18,9 +20,16 @@ public class PriorityQueue<T> {
 		T data;
 		int priority;
 
-		public HeapNode(T data, int priority) {
+		public HeapNode(T data, int priority, int type) {
 			this.data = data;
 			this.priority = priority;
+			if (type == 0) {
+				min = true;
+				max = false;
+			} else {
+				max = true;
+				min = false;
+			}
 		}
 	}
 
@@ -53,4 +62,81 @@ public class PriorityQueue<T> {
 		return parent;
 	}
 
+	public void insert(HeapNode node) {
+		if (size == capacity) {
+			resize();
+		}
+		heap[size] = node;
+		moveUp(size);
+		size++;
+	}
+
+	private void moveUp(int index) {
+		int parent = getParent(index);
+		if (min) {
+			if (parent != -1 && heap[index].priority < heap[parent].priority) {
+				swap(heap[index], heap[parent]);
+				moveUp(parent);
+			}
+		} else if (max) {
+			if (parent != -1 && heap[index].priority > heap[parent].priority) {
+				swap(heap[index], heap[parent]);
+				moveUp(parent);
+			}
+		}
+	}
+
+	private void swap(HeapNode heapNode, HeapNode heapNode2) {
+		HeapNode temp = heapNode;
+		heapNode = heapNode2;
+		heapNode2 = temp;
+
+	}
+
+	private void resize() {
+		int newCapacity = 2 * this.capacity;
+		HeapNode[] newHeap = new HeapNode[newCapacity];
+		for (int i = 0; i < this.heap.length; i++) {
+			newHeap[i] = this.heap[i];
+		}
+		this.heap = newHeap;
+
+	}
+
+	public HeapNode extractTop() throws Exception {
+		HeapNode top;
+		if (size == 0) {
+			throw new Exception("priority is empty");
+		}
+		top = heap[0];
+		heap[0] = heap[size - 1];
+		size--;
+		moveDown(0);
+		return top;
+
+	}
+
+	private void moveDown(int i) {
+		int left = getLeft(i);
+		int right = getRight(i);
+		if (min) {
+			if (left != -1 && heap[left].priority < heap[i].priority) {
+				swap(heap[left], heap[i]);
+				moveDown(left);
+			} else if (right != -1 && heap[right].priority < heap[i].priority) {
+				swap(heap[right], heap[i]);
+				moveDown(right);
+			}
+
+		} else if (max) {
+			if (left != -1 && heap[left].priority > heap[i].priority) {
+				swap(heap[left], heap[i]);
+				moveDown(left);
+			} else if (right != -1 && heap[right].priority > heap[i].priority) {
+				swap(heap[right], heap[i]);
+				moveDown(right);
+			}
+
+		}
+	}
 }
