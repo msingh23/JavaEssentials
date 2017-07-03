603788fb2af62c7a57be9fdcefae4d24486bc79f
manu singh <manusingh.infinity@gmail.com>
2017-07-03 18:26:54 -0400

    RotateArray

---
+++ src/main/java/pramp/RotatedArray.java
@@ -0,0 +1,125 @@
+/**
+ * 
+ */
+package pramp;
+
+/**
+ * @author manu
+ *
+ */
+import java.io.*;
+import java.util.*;
+
+class RotatedArray {
+
+	public int getIndexShifterArray(int[] inp, int num) {
+		int start = 0;
+		int end = inp.length - 1;
+		int mid = start + (end - start) / 2;
+
+		while (start <= end) {
+			if (inp[mid] == num)
+				return mid;
+			else if (inp[start] <= inp[mid] && inp[mid] <= inp[end]) 
+			{
+				end = mid - 1;
+			} else 
+			{
+				start = mid + 1;
+			}
+			mid = start + (end - start) / 2;
+		}
+		return -1;
+	}
+
+	static int shiftedArrSearch(int[] shiftArr, int num) {
+
+		int pivot = getPivotIndx(shiftArr);
+		
+		if (pivot == -1)
+			return binarySearchIndex(shiftArr, 0, shiftArr.length - 1, num);;
+		if ( num >= shiftArr[pivot] && num <= shiftArr[shiftArr.length - 1]) 
+		{
+			return binarySearchIndex(shiftArr, pivot, shiftArr.length - 1, num);
+		} else
+		{
+			return binarySearchIndex(shiftArr, 0, pivot - 1, num);
+		}
+
+	}
+
+	private static int binarySearchIndex(int[] inp, int start, int end, int num) {
+
+		int mid = start + (end - start) / 2;
+
+		while (start <= end) {
+			if (inp[mid] == num)
+			{
+				return mid;
+			} else if (num < inp[mid])
+			{
+				end = mid - 1;
+			} else
+			{
+				start = mid + 1;
+			}
+			mid = start + (end - start) / 2;
+		}
+
+		return -1;
+	}
+
+	public static int getPivotIndx(int[] inp) {
+
+		int start = 0;
+		int end = inp.length - 1;
+
+		while (start <= end) {
+
+			int mid = start + (end - start) / 2;
+
+			if (mid != 0 && inp[mid - 1] > inp[mid])
+			{
+				return mid;
+			}
+			else if (inp[0] > inp[mid]) 
+			{
+				end = mid - 1;
+			} else 
+			{
+				start = mid + 1;
+			}
+		}
+		return -1;
+	}
+
+	public static void main(String[] args) {
+
+	}
+
+}
+
+/*
+ * 
+ * [1,2,3,4,5]
+ * 
+ * [2,3,4,5,1] [3,4,5,1,2] [4,5,1,2,3] [5,1,2,3,4]
+ * 
+ * [2, 4, 5, 9, 12, 17]
+ * 
+ * no = 4 shiftd = [9, 12, 17, 19, [2], 4, 5] int first = 19 int last = 5
+ * 
+ * [9, 12, 17, 2, 4, 5],
+ * 
+ * [15, 16, 17, 2, 4, 5, 12],
+ * 
+ * [12, 15, 16, 17, 2, 4, 5],
+ * 
+ * 
+ * first , inp[mid] 9 , 17 inp[mid], last 17, 5 start = 3 end = 6 mid = 4
+ * 
+ * 
+ * 
+ * [start pivot] last
+ * 
+ */
---
+++ src/test/java/pramp/RotatedArrayTest.java
@@ -0,0 +1,47 @@
+package pramp;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import junit.framework.Assert;
+
+public class RotatedArrayTest {
+
+	public RotatedArray handler;
+	int[][] inp;
+	int [] out;
+	int num;
+
+	@Before
+	public void setUp() throws Exception {
+		handler = new RotatedArray();
+		inp = new int[][] { 
+							{9, 12, 17, 2, 4, 5},
+							{12, 17, 2, 4, 5 ,9},
+							{17, 2, 4, 5 ,9, 12},
+							{5, 9, 12, 17, 2, 4},
+							{4, 5, 9, 12, 17, 2},
+							{4, 5, 9, 12, 17, 2 ,3 },
+							{3, 4, 5, 9, 12, 17, 2 },
+							{5, 9, 12, 17, 2 ,3, 4 },
+							{9, 12, 17, 2 ,3, 4, 5 },
+							{12, 17, 2 ,3, 4, 5, 9 },
+							{17, 2 ,3, 4, 5, 9, 12 },
+							{2 ,3, 4, 5, 9, 12}};
+		
+		num = 2;
+		out = new int[]{3,2,1,4,5,5,6,4,3,2,1,0};
+	}
+
+	@Test
+	public void test() {
+		for(int i = 0 ; i < inp.length ; i++){
+			//System.out.println(i);
+			Assert.assertEquals(out[i], handler.shiftedArrSearch(inp[i], num));
+			
+		}
+	}
+
+}
