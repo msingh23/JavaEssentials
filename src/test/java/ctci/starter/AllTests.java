4ab06abf27f6a27eb558bc9a8605713bba462e3b
manu singh <manusingh.infinity@gmail.com>
2017-07-06 02:50:48 -0400

    all test

--- src/test/java/ctci/starter/AllTests.java
+++ src/test/java/ctci/starter/AllTests.java
@@ -54,6 +54,7 @@ import ctci.chap5.FlipBitTest;
 import ctci.chap5.InsertionTest;
 import ctci.chap5.NextNumberTest;
 import ctci.chap5.PairwiseSwapTest;
+import ctci.chap8.RobotInGridTest;
 import ctci.chap8.TripleStep;
 import ctci.chap8.TripleStepTest;
 import pramp.ArrayIndexElementEqualityTest;
@@ -83,7 +84,7 @@ import pramp.SmallestSubstringContainingArrayTest;
 		ArrayIndexElementEqualityTest.class, FindTheDuplicatesTest.class, AwardBudgetCutsTest.class,
 		BracketMatchTest.class, FlattenDictionaryTest.class, BasicRegexParserTest.class, IslandCountTest.class,
 		RotatedArrayTest.class, NextNumberTest.class, ConversionTest.class, PairwiseSwapTest.class,
-		TripleStepTest.class })
+		TripleStepTest.class, RobotInGridTest.class })
 
 public class AllTests {
 
