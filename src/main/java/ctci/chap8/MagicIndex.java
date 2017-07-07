6410e017412a4a0a505f20a67ed525a11eee46e0
manu singh <manusingh.infinity@gmail.com>
2017-07-07 12:31:35 -0400

    Magic Index

--- src/main/java/ctci/chap8/MagicIndex.java
+++ src/main/java/ctci/chap8/MagicIndex.java
@@ -3,6 +3,13 @@ package ctci.chap8;
 public class MagicIndex {
 	
 	
+	/*
+	 * 
+	 * 1,3,4,5,6,7,7,7,7
+	 * 0 1 2 3 4 5 6 7 8
+	 * 
+	 */
+	
 	public int getIndex(int [] inp)
 	{
 		int start = 0 ; 
@@ -25,6 +32,49 @@ public class MagicIndex {
 			}
 					
 		}
+		return -1;
+	}
+	
+	public int getIndexNonUnique(int [] inp)
+	{
+		return getIndexNonUnique(inp, 0, inp.length -1);
+	}
+
+	private int getIndexNonUnique(int[] inp, int start, int end) {
+
+		if(start<=end)
+		{
+			int mid = start + (end-start) / 2;
+			if(inp[mid] == mid)
+			{
+				return mid;
+			}
+			
+			/*
+			 * search left of mid but search only from indexed start to min(inp[mid], mid-1)
+			 */
+			
+			int newEnd = Math.min(mid -1, inp[mid]);
+			int left = getIndexNonUnique(inp, start, newEnd);
+			
+			
+			
+			/*
+			 * search right of mid but search only from index max(inp[mid], mid+1) to end
+			 */
+			int newstart=-1;
+			int right = -1;
+			if(left==-1)
+			{
+				newstart = Math.max(mid+1, inp[mid]);
+				right = getIndexNonUnique(inp, newstart, end);
+			}else{
+				return left;
+			}
+			
+			return right;
+		}
+		
 		return -1;
 	}
 
--- src/test/java/ctci/chap8/MagicIndexTest.java
+++ src/test/java/ctci/chap8/MagicIndexTest.java
@@ -11,12 +11,14 @@ public class MagicIndexTest {
 
 	public MagicIndex handler;
 	int [] inp;
+	int [] inp2;
 	int index ;
 	
 	@Before
 	public void setUp() throws Exception {
 		handler = new MagicIndex();
 		inp = new int[] {-1,0,1,2,3,5};
+		inp2 = new int[] {1,3,4,5,6,7,7,7,7};
 		index = 5;
 		
 	}
@@ -24,6 +26,7 @@ public class MagicIndexTest {
 	@Test
 	public void test() {
 		Assert.assertEquals(5, handler.getIndex(inp));
+		Assert.assertEquals(7, handler.getIndexNonUnique(inp2));
 	}
 
 }
