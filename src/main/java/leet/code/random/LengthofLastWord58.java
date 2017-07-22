95e16672741e25e8c1a8ed913bf7c0faba2a193e
manu singh <manusingh.infinity@gmail.com>
2017-07-21 21:23:02 -0400

    length of last word

---
+++ src/main/java/leet/code/random/LengthofLastWord58.java
@@ -0,0 +1,25 @@
+/**
+ * 
+ */
+package leet.code.random;
+
+/**
+ * @author manu
+ *
+ */
+public class LengthofLastWord58 {
+
+	public int lengthOfLastWord(String s) {
+        s= s.trim();
+        int len = 0;
+        int index = s.length()-1;
+        while( index>= 0 && s.charAt(index--)!=' ') len++;
+        
+        //if(index<0) return 0;
+        return len;
+        
+        
+    }
+	
+	
+}
