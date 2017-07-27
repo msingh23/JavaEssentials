b072573fd46a7e4a03c7a395d8af4a8d4cdc0989
manu singh <manusingh.infinity@gmail.com>
2017-07-27 09:08:57 -0400

    restore ip

--- src/main/java/leet/code/random/RestoreIPAddresses93.java
+++ src/main/java/leet/code/random/RestoreIPAddresses93.java
@@ -11,14 +11,12 @@ import java.util.List;
  *
  */
 
-
 public class RestoreIPAddresses93 {
 
 	/*
-	0(2^n)
-	List<String> ans = new ArrayList<String>();
-
-	public List<String> restoreIpAddresses(String s) {
+	 * n!
+	 * */
+	public List<String> restoreIpAddresses2(String s) {
 
 		if (s != null && s.length() > 3 && s.length() < 13) {
 
@@ -34,10 +32,8 @@ public class RestoreIPAddresses93 {
 
 	public void getIp(String s, int start, StringBuffer buff, int len, int dots, String domain, List<String> out) {
 
-		if (dots == 3 && buff.charAt(buff.length() - 1) != '.' && buff.length() == s.length() + 3) {
+		if (dots == 3 && buff.charAt(buff.length()-1)!='.' && buff.length()==s.length()+3) {
 			out.add(buff.toString());
-			// System.out.println(buff);
-			// check if the ip is valid
 			return;
 		}
 
@@ -45,7 +41,8 @@ public class RestoreIPAddresses93 {
 
 			String digit = (s.substring(i, i + 1));
 			buff.append(digit);
-			if (Integer.valueOf(domain + digit) <= 255 && ((domain.equals("") || false)  || Integer.valueOf(domain + digit) > Integer.valueOf(digit))) {
+			if (Integer.valueOf(domain + digit) <= 255 && ( (domain.equals("") || false)  || Integer.valueOf(domain + digit) > Integer.valueOf(digit)) ) {
+
 
 				if (dots < 3) {
 					getIp(s, i + 1, buff.append("."), len, dots + 1, "", out);
@@ -55,17 +52,52 @@ public class RestoreIPAddresses93 {
 
 			}
 			buff.deleteCharAt(buff.length() - 1);
+		}
 
-			
+	}
+   
+	List<String> ans = new ArrayList<String>();
+
+	/*
+	 * 
+	 * 0(3^4)
+	 */
+	
+	public List<String> restoreIpAddresses(String s) {
+
+		if (s.length() <= 12 && s.length() > 3)
+			restoreIpAddressesHelper(s, ans, "", 0, 0);
+		System.out.println(ans);
+		return ans;
+
+	}
+
+	public void restoreIpAddressesHelper(String s, List<String> out, String curr, int index, int count) {
+		
+		if(count>4){
+			return;
+		}
+		
+		if (!curr.endsWith(".") && curr.length() == s.length() + 3) {
+			out.add(curr.toString());
+		}
+
+		for (int i = 1; i < 4; i++) {
+			if (index + i > s.length())
+				break;
+			String str = s.substring(index, index + i);
+			Integer val = Integer.valueOf(str);
+			if (str.startsWith("0") && str.length()>1  || val >= 256 )
+				break;
+			restoreIpAddressesHelper(s, out, curr + str + (count >= 3 ? "" : "."), index + i, count + 1);
 
 		}
 
 	}
-	
-	*/
 
 	public static void main(String[] args) {
 		RestoreIPAddresses93 ip = new RestoreIPAddresses93();
-		//ip.restoreIpAddresses("686431045685");
+		//ip.restoreIpAddresses2("25525511135");
+		ip.restoreIpAddresses("0000");
 	}
 }
