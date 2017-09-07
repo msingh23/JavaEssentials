977c049a2cdd57d751ad7aaecd8d1c743c1434a5
manu singh <manusingh.infinity@gmail.com>
2017-09-07 18:03:08 -0400

    treenode

---
+++ src/main/java/leet/code/random/TreeNode.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+package leet.code.random;
+
+/**
+ * @author manu
+ *
+ */
+public class TreeNode {
+
+	int val;
+	TreeNode left;
+	TreeNode right;
+
+	TreeNode(int x) {
+		val = x;
+	}
+}
