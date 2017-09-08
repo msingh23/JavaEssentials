d1bb2d3454a50215e961273676555031ad1893ad
manu singh <manusingh.infinity@gmail.com>
2017-09-08 00:23:27 -0400

    max path sum

--- src/main/java/leet/code/random/LC124BinaryTreeMaximumPathSum.java
+++ src/main/java/leet/code/random/LC124BinaryTreeMaximumPathSum.java
@@ -18,17 +18,10 @@ public class LC124BinaryTreeMaximumPathSum {
 	    {
 	        if(root==null) return 0;
 	        
-	        int left = maxPathSumHelper(root.left);
-	        int right = maxPathSumHelper(root.right);
+	        int left = Math.max(maxPathSumHelper(root.left),0);
+	        int right = Math.max(maxPathSumHelper(root.right),0);
 	        int childMax = Math.max(left, right);
-	        //consider both child and one of the larger childs.
-	        max = Math.max(Math.max(childMax+root.val, root.val + left+ right ), max);
-	        //if both childs are negative then ignore them
-	        if(left <0 && right <0) 
-	        {
-	            max= Math.max(root.val, max);
-	            return root.val;
-	        }
+	        max = Math.max( root.val+left+right, max);
 	        return childMax + root.val;
 	        
 	    }
