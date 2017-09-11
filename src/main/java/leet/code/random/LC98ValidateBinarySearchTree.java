42f53468226ef37a09eb09e4ab0ea538745d7c26
manu singh <manusingh.infinity@gmail.com>
2017-09-11 19:16:15 -0400

    validate bst

--- src/main/java/leet/code/random/LC98ValidateBinarySearchTree.java
+++ src/main/java/leet/code/random/LC98ValidateBinarySearchTree.java
@@ -3,13 +3,16 @@
  */
 package leet.code.random;
 
+import java.util.Stack;
+
 /**
  * @author manu
  *
  */
 public class LC98ValidateBinarySearchTree {
 
-	public boolean isValidBST(TreeNode root) {
+	 /*
+    public boolean isValidBST(TreeNode root) {
         
 	       return isValidBST(root,Long.MAX_VALUE, Long.MIN_VALUE);
 	    }
@@ -24,5 +27,54 @@ public class LC98ValidateBinarySearchTree {
 	        }
 	        return false;
 	    }
+    */
+    /*
+    public boolean isValidBST(TreeNode root) {
+        
+      if(root==null) return true;
+       return isValidBST(root.left,root, null) && isValidBST(root.right,null, root);
+    }
+    
+    public boolean isValidBST(TreeNode root, TreeNode max, TreeNode min)
+    {
+        if(root==null) return true;
+        
+        if( (max==null || root.val <max.val) && (min == null || root.val>min.val) )
+        {
+            return isValidBST(root.left, root, min) &&  isValidBST (root.right, max, root );          
+        }
+        return false;
+    }
+    */
+    
+    public boolean isValidBST(TreeNode root) {
+        
+        Stack<TreeNode> st = new Stack<TreeNode>();
+        TreeNode pre = null;
+        
+        while(root!=null || !st.isEmpty())
+        {
+            
+            while(root!=null)
+            {
+                st.add(root);
+                root = root.left;
+            }
+            
+            if(!st.isEmpty())
+            {
+                 root = st.pop();
+                 if(pre!=null && pre.val >= root.val) return false;
+                 //System.out.println(root.val);
+                 pre = root;
+                 root = root.right;
+            }
+           
+        }
+        
+        return true;
+       
+    }
+	    
 	    
 }
