19de7bf7c6944b6b59245aa15e37448606874fbd
manu singh <manusingh.infinity@gmail.com>
2017-09-12 00:29:44 -0400

    LCA

---
+++ src/main/java/leet/code/random/LC236LowestCommonAncestorofaBinaryTree.java
@@ -0,0 +1,24 @@
+/**
+ * 
+ */
+package leet.code.random;
+
+/**
+ * @author manu
+ *
+ */
+public class LC236LowestCommonAncestorofaBinaryTree {
+
+public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        
+        if(root == null) return null;
+        if(root == p || root == q) return root;
+        
+        TreeNode left = lowestCommonAncestor(root.left, p, q);
+        TreeNode right = lowestCommonAncestor(root.right, p, q);
+        
+        if(left!= null && right != null) return root;
+        return left!=null? left: right;
+    }
+	
+}
