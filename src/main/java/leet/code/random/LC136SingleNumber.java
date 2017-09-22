b88aca5326386a431c57e4cf4120fff0f418ff3a
manu singh <manusingh.infinity@gmail.com>
2017-09-21 21:49:16 -0400

    single rain water

---
+++ src/main/java/leet/code/random/LC136SingleNumber.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+package leet.code.random;
+
+/**
+ * @author manu
+ *
+ */
+public class LC136SingleNumber {
+	public int singleNumber(int[] nums) {
+
+		int xor = nums[0];
+		for (int i = 1; i < nums.length; i++)
+			xor ^= nums[i];
+
+		return xor;
+	}
+}
