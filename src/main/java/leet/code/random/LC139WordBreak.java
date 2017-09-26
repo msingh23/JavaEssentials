1c7a508f05ce4f20ec8baf41176a7fbc15f28096
manu singh <manusingh.infinity@gmail.com>
2017-09-25 21:40:11 -0400

    word break

--- src/main/java/leet/code/random/LC139WordBreak.java
+++ src/main/java/leet/code/random/LC139WordBreak.java
@@ -2,34 +2,41 @@ package leet.code.random;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
+import java.util.Map;
 import java.util.Set;
 
 public class LC139WordBreak {
-	public boolean wordBreak(String s, List<String> wordDict)
-    {
-        Set<String> set = new HashSet<String>();
-        for(String word : wordDict ) set.add(word);
-        
-        return wordBreakHelper(s, set);
+	public boolean wordBreak(String s, List<String> wordDict) {
+        Map<String, Boolean> map = new HashMap<String, Boolean>();
+        for(String word : wordDict) map.put(word, true);
+        return wordBreak(s, map);
     }
-public boolean wordBreakHelper(String s, Set<String> set) {
-		if(s.equals("") || set.contains(s)) return true;
-
-		boolean ans = false;
-		for (int i = 1; i < s.length(); i++) {
-			String s1 = s.substring(0, i);
-			String s2 = s.substring(i);
-//			System.out.println(s1);
-//			System.out.println(s2);
-//			System.out.println();
-			if ( set.contains(s1) && wordBreakHelper(s2, set)) {
-				return true;
-			} 
-		}
-		return ans;
-	}
+    
+    public boolean wordBreak(String s, Map<String, Boolean> map)
+    {
+        if(null == s ) return false;
+        if(!map.containsKey(s))
+        {
+            int n = s.length();
+            map.put(s, false);
+            for(int i = 1; i<n ; i++)
+            {
+                String left = s.substring(0,i);
+                String right = s.substring(i,n);
+                if(map.getOrDefault(left, false) &&  wordBreak(right, map) )
+                {
+                    map.put(s, true);
+                    break;
+                }
+               
+            }
+           
+        }
+        return map.get(s);
+    }
 	
 	public static void main(String[] args) {
 		LC139WordBreak lc = new LC139WordBreak();
