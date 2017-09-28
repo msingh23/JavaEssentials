f3b35fd87902f12edc2b8909bb6ba0dc23be9279
manu singh <manusingh.infinity@gmail.com>
2017-09-27 20:44:29 -0400

    Group Anagrams

--- src/main/java/leet/code/random/LC49GroupAnagrams.java
+++ src/main/java/leet/code/random/LC49GroupAnagrams.java
@@ -14,6 +14,7 @@ import java.util.Map;
  */
 public class LC49GroupAnagrams {
 
+	/*
 	public List<List<String>> groupAnagrams(String[] strs) {
 
 		List<List<String>> out = new ArrayList<List<String>>();
@@ -39,5 +40,29 @@ public class LC49GroupAnagrams {
 		return new ArrayList<List<String>>(map.values());
 
 	}
-
+*/
+	 public static List<List<String>> groupAnagrams(String[] strs) { 
+		   int[] prime = {2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97, 101, 103};//最多10609个z
+		    
+		            List<List<String>> res = new ArrayList<>();
+		            HashMap<Integer, Integer> map = new HashMap<>();
+		            for (String s : strs) {
+		                int key = 1;
+		                for (char c : s.toCharArray()) {
+		                    key *= prime[c - 'a'];
+		                }
+		                List<String> t;
+		                if (map.containsKey(key)) {
+		                    t = res.get(map.get(key));
+		                } else {
+		                    t = new ArrayList<>();
+		                    res.add(t);
+		                    map.put(key, res.size() - 1);
+		                }
+		                t.add(s);
+		            }
+		            return res;
+		    }
+	
+	
 }
