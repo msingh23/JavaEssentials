f628c88908e55a01abc9c91dfff2d186d8b69cdc
manu singh <manusingh.infinity@gmail.com>
2017-10-01 15:14:14 -0400

    min stack

--- src/main/java/leet/code/random/LC155MinStack.java
+++ src/main/java/leet/code/random/LC155MinStack.java
@@ -11,7 +11,9 @@ import java.util.Stack;
  */
 public class LC155MinStack {
 
+	
 	  /** initialize your data structure here. */
+	/*
     Stack<Integer> valStack;
     Stack<Integer> minStack;
     public LC155MinStack() {
@@ -48,4 +50,52 @@ public class LC155MinStack {
             return minStack.peek();
           return -1;
     }
+    */
+	
+
+    /** initialize your data structure here. */
+    Stack<Integer> valStack;
+    Integer min;
+    public LC155MinStack() {
+        
+        valStack = new Stack<Integer>();
+        //minStack = new Stack<Integer>();
+        min = Integer.MAX_VALUE;
+    }
+    
+    public void push(int x) {
+        
+        if(x <= min && x!= Integer.MAX_VALUE)
+        {
+            valStack.push(min);
+            min = x;
+        }
+        valStack.push(x);
+    }
+    
+    public void pop() {
+        
+        if(valStack.peek().equals(min) )
+        {
+          //   System.out.println("EQUALS: ");
+             valStack.pop();
+             min = valStack.pop();
+        }
+        else valStack.pop();
+        // System.out.println("pop: " + valStack+ " minVal: " + min);
+    }
+    
+    public int top() {
+        // System.out.println("top: " + valStack+ " minVal: " + min);
+        if(!valStack.isEmpty())
+            return valStack.peek();
+        return -1;
+    }
+    
+    public int getMin() {
+         //System.out.println("min: " + valStack+ " minVal: " + min);
+          return min;
+    }
+
+    
 }
