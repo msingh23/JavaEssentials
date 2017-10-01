521fb54ccbad462cbbfbdde6d88ac2488d525b5a
manu singh <manusingh.infinity@gmail.com>
2017-10-01 18:28:47 -0400

    merge two

---
+++ src/main/java/leet/code/random/LC617MergeTwoBinaryTrees.java
@@ -0,0 +1,31 @@
+/**
+ * 
+ */
+package leet.code.random;
+
+/**
+ * @author manu
+ *
+ */
+public class LC617MergeTwoBinaryTrees {
+
+	 public TreeNode mergeTrees(TreeNode t1, TreeNode t2) {
+	      
+	        TreeNode root  = null;
+	        if(t1 == null && t2 == null) return null;
+	        else if(t2 == null) 
+	        {
+	            return t1;
+	        }
+	        else if(t1 == null){
+	           return t2;
+	        }else{
+	            root = new TreeNode(t1.val + t2.val);
+	            root.left = mergeTrees(t1.left, t2.left);
+	            root.right = mergeTrees(t1.right, t2.right);
+	        }
+	        
+	        return root;
+	        
+	    }
+}
