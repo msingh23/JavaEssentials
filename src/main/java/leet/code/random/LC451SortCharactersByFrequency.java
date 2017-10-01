562c48f2b80048ef7cd06902664f66a633226cbd
manu singh <manusingh.infinity@gmail.com>
2017-10-01 11:51:23 -0400

    sort character

--- src/main/java/leet/code/random/LC451SortCharactersByFrequency.java
+++ src/main/java/leet/code/random/LC451SortCharactersByFrequency.java
@@ -3,6 +3,7 @@
  */
 package leet.code.random;
 
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.PriorityQueue;
@@ -14,70 +15,79 @@ import java.util.Set;
  */
 public class LC451SortCharactersByFrequency {
 	
-public String frequencySort(String s) {
-        
-        
-        // int [] upper = new int [26];
-        // int [] lower = new int [26];
-        Map<Character, Integer> map = new HashMap<Character, Integer>();
-        PriorityQueue<Object []> pq = new PriorityQueue<Object[]>( (o1, o2) -> (int)o2[1] - (int) o1[1] );
-        for(Character ch  : s.toCharArray())
-        {
-            if(!map.containsKey(ch))
-            {
-                map.put(ch, 0);
-            }
-            
-            map.put(ch, map.get(ch)+1);
-        }
-       
-        /*
-        for(Character ch  : s.toCharArray())
-        {
-            if(ch>=65  && ch<=90)
-                upper[ch-'A']++;
-            if(ch>=97 && ch<=122)
-                lower[ch-'a']++;
-        }
-       
-        for(int i = 0; i < 26; i++)
-        {
-            if(upper[i] > 0)
-                pq.add(new Object[]{'A' + i, upper[i]});
-            if(lower[i] > 0)
-                pq.add(new Object[]{'a' + i, lower[i]});
-        }
-        */
-        
-        Set<Character> keys = map.keySet();
-        for(Character key : keys)
-             pq.add(new Object[]{key, map.get(key)});
-        /*
-        Iterator< Character> iter = keys.iterator();
-        
-        while(iter.hasNext())
-        {
-            char key = iter.next();
-            pq.add(new Object[]{key, map.get(key)});
-        }
-        */
-        
-        char [] out = new char [s.length()];
-        int i = 0;
-        while(!pq.isEmpty())
-        {
-            Object[] obj = pq.poll();
-            Character ch = (Character) obj[0];
-            int count  = (int) obj[1];
-            
-            while(count>0)
-            {
-                out[i++] = (char) ch;
-                count--;
-            }
-        }
-        
-        return new String(out);
-    }
+	 public String frequencySort(String s) {
+	        
+	        Map<Character, Integer> map = new HashMap<Character, Integer>();
+	        PriorityQueue<Object []> pq = new PriorityQueue<Object[]>( (o1, o2) -> (int)o2[1] - (int) o1[1] );
+	        for(Character ch  : s.toCharArray())
+	        {
+	            if(!map.containsKey(ch))
+	            {
+	                map.put(ch, 0);
+	            }
+	            
+	            map.put(ch, map.get(ch)+1);
+	        }
+	       
+	        Set<Character> keys = map.keySet();
+	        for(Character key : keys)
+	             pq.add(new Object[]{key, map.get(key)});
+	        
+	        char [] out = new char [s.length()];
+	        int i = 0;
+	        while(!pq.isEmpty())
+	        {
+	            Object[] obj = pq.poll();
+	            Character ch = (Character) obj[0];
+	            int count  = (int) obj[1];
+	            
+	            while(count>0)
+	            {
+	                out[i++] = (char) ch;
+	                count--;
+	            }
+	        }
+	        
+	        return new String(out);
+	    }
+	 
+     public String frequencySort2(String s) 
+     {
+         int [] freq = new int [256];
+         Arrays.fill(freq, 0);
+         int maxBucket = 0;
+         for(Character ch : s.toCharArray())
+         {
+             freq[ch]++;
+             maxBucket = Math.max(freq[ch], maxBucket);
+         }
+         StringBuffer bucket [] = new StringBuffer [maxBucket+1];
+         
+         for(int i = 0 ;i < 256; i++)
+         {
+             if(freq[i] > 0)
+             {
+                 char ch = (char) i;
+                 bucket[freq[i]] = bucket[freq[i]] == null ? new StringBuffer(String.valueOf(ch)) : bucket[freq[i]].append(String.valueOf(ch));
+             }
+         }
+         int j = 0;
+         char [] out = new char[ s.length() ];
+         
+         for(int i = maxBucket; i>0 ; i--)
+         {
+             if(bucket[i] == null ) continue;
+             
+             for(Character ch : bucket[i].toString().toCharArray())
+             {
+                 int count = i;
+                 while(count-- > 0)
+                 {
+                     out[j++] = ch;
+                 }
+             }
+         }
+         return new String(out);
+     }
 
 }
