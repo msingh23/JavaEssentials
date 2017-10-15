081df9d303da435c30cc038222069ddb5eb06d69
manu singh <manusingh.infinity@gmail.com>
2017-10-15 19:24:02 -0400

    coin change

--- src/main/java/leet/code/random/LC518CoinChange2.java
+++ src/main/java/leet/code/random/LC518CoinChange2.java
@@ -9,21 +9,28 @@ public class LC518CoinChange2 {
 	 * 
 	 */
 
-	public int change(int amount, int[] coins) {
-		int n = coins.length;
-		int dp[][] = new int[n + 1][amount + 1];
-		dp[0][0] = 1;
-		for (int i = 1; i < n + 1; i++) {
-			dp[i][0] = 1;
-			for (int j = 1; j <= amount; j++) {
-				if ((j - coins[i - 1] >= 0))
-					dp[i][j] = dp[i - 1][j] + dp[i][j - coins[i - 1]];
-				else
-					dp[i][j] = dp[i - 1][j];
-			}
-		}
-		return dp[n][amount];
-	}
+	  public int change(int amount, int[] coins) {
+	        
+	        int r = coins.length+1;
+	        int c = amount+1;
+	        int dp [] []  = new int [r][c]; 
+	        
+	        for(int i = 0 ;i <r; i++)
+	            dp[i][0] = 1;
+	        
+	            for(int i = 1 ; i<r ;i++)
+	            {
+	                int coin = coins[i-1];
+	                for(int sum = 1; sum< c; sum++)
+	                {
+	                    dp[i][sum] =  dp[i-1][sum] + (sum<coin ? 0 : dp[i][sum-coin]);
+	                }
+	                System.out.println();
+	            }
+	        
+	        return dp[r-1][c-1];
+	        
+	    }
 
 	public static void main(String[] args) {
 		LC518CoinChange2 lc = new LC518CoinChange2();
