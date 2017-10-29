efef71d1ad1fcae80c8d59218764f70944e3b224
manu singh <manusingh.infinity@gmail.com>
2017-10-29 17:39:14 -0400

    even odd

---
+++ src/main/java/leet/code/random/LC328OddEvenLinkedList.java
@@ -0,0 +1,26 @@
+/**
+ * 
+ */
+package leet.code.random;
+
+/**
+ * @author manu
+ *
+ */
+public class LC328OddEvenLinkedList {
+	public ListNode oddEvenList(ListNode head) {
+	    if (head != null) {
+	    
+	        ListNode odd = head, even = head.next, evenHead = even; 
+	    
+	        while (even != null && even.next != null) {
+	            odd.next = odd.next.next; 
+	            even.next = even.next.next; 
+	            odd = odd.next;
+	            even = even.next;
+	        }
+	        odd.next = evenHead; 
+	    }
+	    return head;
+	}
+}
