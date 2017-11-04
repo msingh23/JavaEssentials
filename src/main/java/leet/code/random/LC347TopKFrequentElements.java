e2cdb8f7cf3de3ca3710f08b47924c9d2f80d9ad
manu singh <manusingh.infinity@gmail.com>
2017-11-04 13:31:50 -0400

    top k

--- src/main/java/leet/code/random/LC347TopKFrequentElements.java
+++ src/main/java/leet/code/random/LC347TopKFrequentElements.java
@@ -7,7 +7,6 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.PriorityQueue;
 
 /**
  * @author manu
@@ -16,6 +15,7 @@ import java.util.PriorityQueue;
 public class LC347TopKFrequentElements {
 
 	
+	/*
 	public List<Integer> topKFrequent(int[] nums, int k) {
         Map<Integer, Integer> freq = new HashMap<Integer, Integer>();
         List<Integer> out = new ArrayList<Integer>();
@@ -39,4 +39,44 @@ public class LC347TopKFrequentElements {
         }
         return  out;
     }
+    */
+	
+	 public static List<Integer> topKFrequent(int[] nums, int k) {
+	        List<Integer>[] buck = new List[nums.length+1];
+	        List<Integer> out = new ArrayList<Integer>();
+	         Map<Integer, Integer> freq = new HashMap<Integer, Integer>();
+	         for(int n : nums)
+	        {
+	            freq.put(n , freq.getOrDefault(n, 0)+1);
+	                
+	        }
+	        
+	         for(Integer key : freq.keySet())
+	        {
+	            int count = freq.get(key);
+	             if(buck[count]==null)
+	                  buck[count] = new ArrayList<>(); 
+	             buck[count].add(key);
+	             
+	        }
+	        
+	        for(int i = nums.length;i>=0 ;i --)
+	        {
+	            if(buck[i]!=null )
+	            {
+	                for(Integer key : buck[i])
+	                {
+	                    if(out.size() == k) break;
+	                    out.add(key);
+	                }
+	            }
+	        }
+	        
+	        return out;
+	        
+	    }
+	 
+	 public static void main(String[] args) {
+		System.out.println(topKFrequent(new int []{1,1,1,2,2,3}, 2));
+	}
 }
